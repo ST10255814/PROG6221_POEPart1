9a814b5ab467a63fca4717e06ae4defd4fd47947
Jayden Larkins <ST10255814@vcconnect.edu.za>
2024-02-28 15:15:31 +0200

    "Commit V2"

---
+++ Methods.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poePart1
+{
+    internal class Methods
+    {
+        public void addIngrediants(List<string> allIngrediants)
+        {
+            string ingrediantName, unitOfMeasure = "", temp; 
+            int choice;
+
+            Console.WriteLine("How many ingrediants does the recipe contain? ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < quantity; i++)
+            {
+                Console.WriteLine("Enter ingrediant name: ");
+                ingrediantName = Console.ReadLine();
+
+                Console.WriteLine("How much of the ingrediant is needed? ");
+                quantity = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("What is the unit of measurement?\n1: Tablespoon/s\n2: Teaspoon/s\n3: Cup/s");
+                choice = Convert.ToInt32(Console.ReadLine());
+                choice --;
+
+                switch (choice)
+                {
+                    case 0:
+                        if(quantity > 1)
+                        {
+                            unitOfMeasure = "Tablespoons";
+                        }
+                        else
+                        {
+                            unitOfMeasure = "Tablespoon";
+                        }
+                        break;
+
+                    case 1:
+                        if (quantity > 1)
+                        {
+                            unitOfMeasure = "Teaspoons";
+                        }
+                        else
+                        {
+                            unitOfMeasure = "Teaspoon";
+                        }
+                        break;
+
+                    case 2: 
+                        if (quantity > 1) 
+                        {
+                            unitOfMeasure = "Cups";
+                        }
+                        else
+                        {
+                            unitOfMeasure = "Cup";
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid option");
+                        break;
+                }
+                temp = ingrediantName + ": " + quantity + " " + unitOfMeasure;
+
+                allIngrediants[i] = temp;
+            }
+        }
+        public void clearRecipe()
+        {
+
+        }
+        public void addSteps(List<string> allSteps)
+        {
+            String stepDescription;
+
+            Console.WriteLine("How many steps are there? ");
+            int noOfSteps = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < noOfSteps; i++)
+            {
+                Console.WriteLine("What is the description of step " + (i + 1));
+                stepDescription = Console.ReadLine();
+
+                allSteps[i] = stepDescription;
+            }
+        }
+        public void displayRecipe(List<string> allIngrediants, List<string> allSteps) 
+        {
+            Console.WriteLine("Recipe:\n\n");
+
+            for (int i = 0; i < allIngrediants.Count; i++)
+            {
+                Console.WriteLine("Ingrediants:\n");
+                Console.WriteLine(allIngrediants[i]);
+            }
+            for (int i = 0; i < allSteps.Count; i++)
+            {
+                Console.WriteLine("Steps:\n");
+                Console.WriteLine("Step " + (i+1) + ": " + allSteps[i]);
+            }
+        }
+        public void scaleRecipe(int scaleFactor) 
+        {
+
+        }
+        public void resetScale(int scaleFactor)
+        {
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> allIngrediants = new List<string>();
+            List<string> allSteps = new List<string>();
+
+            Methods mt = new Methods();
+
+            Console.WriteLine("Welcome! Please select one of the following options to create your recipe: " +
+                "\n1: Add Ingrediants\n2: Add steps\n3: Display recipe\n4: Scale recipe\n5: Reset Scale\n 6: Clear recipe\n7: Exit");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            choice--;
+
+            switch (choice)
+            {
+                case 0:
+                    mt.addIngrediants(allIngrediants);
+                    break;
+
+                case 1:
+                    mt.addSteps(allSteps);
+                    break;
+
+                case 2:
+                    mt.displayRecipe(allIngrediants, allSteps);
+                    break;
+
+                case 3:
+                    Console.WriteLine("What factor would you like to scale the recipe by? E.g 3");
+                    int scaleFactor = Convert.ToInt32(Console.ReadLine());
+
+                    mt.scaleRecipe(scaleFactor);
+                    break;
+
+                case 4:
+                    mt.resetScale(scaleFactor);
+                    break;
+
+            }
         }
     }
 }
