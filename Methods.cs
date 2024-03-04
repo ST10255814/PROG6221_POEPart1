b93e4c46749327befc3e3e6c4db62c3f226fc726
Jayden Larkins <ST10255814@vcconnect.edu.za>
2024-03-04 12:21:10 +0200

    Commit V4

--- Methods.cs
+++ Methods.cs
@@ -8,37 +8,54 @@ namespace poePart1
 {
     internal class Methods
     {
-        int scaleFactor, originalScaleFactor = 1;
-        public void addIngrediants(List<string> allIngrediants)
+        private int scaleFactor; //Global variable used for later calculations 
+        /*
+         * Allows the user to specify the amount of ingrediants and allows for each input and qauntity to be recorded and saved 
+         * into multiple array lists to be displayed and perform calculations on later 
+         */
+        public void addIngrediants(List<string> allIngrediantNames, List<int> allQuantities, List<string> allUnitsOfMeasures)
         {
-            string ingrediantName, unitOfMeasure = "", temp; 
-            int choice;
+            string ingrediantName, unitOfMeasure, temp; 
+            int amount;
 
             Console.WriteLine("How many ingrediants does the recipe contain? ");
             int quantity = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i < quantity; i++)
+            for (int i = 0; i < quantity; i++) //for loop for the amount of ingrediants required 
             {
                 Console.WriteLine("Enter ingrediant name: ");
                 ingrediantName = Console.ReadLine();
 
+                allIngrediantNames.Add(ingrediantName);
+
                 Console.WriteLine("How much of the ingrediant is needed? ");
-                quantity = Convert.ToInt32(Console.ReadLine());
+                amount = Convert.ToInt32(Console.ReadLine());
+
+                allQuantities.Add(amount);
 
                 Console.WriteLine("What is the unit of measurement?");
                 unitOfMeasure = Console.ReadLine();
-                
-                temp = ingrediantName + ": " + quantity + " " + unitOfMeasure;
 
-                allIngrediants[i] = temp;
+                allUnitsOfMeasures.Add(unitOfMeasure);
 
             }
         }
-        public void clearRecipe(List<string> allIngrediants, List<string>allSteps)
+        /*
+         * Clears all array elements to allow for an entire new recipe to be entered 
+         */
+        public void clearRecipe(List<string> allIngrediantNames, List<string>allSteps, List<int> allQuantities, 
+            List<string> allUnitsOfMeasures)
         {
-            allIngrediants.Clear();
+            allIngrediantNames.Clear();
             allSteps.Clear();
+            allQuantities.Clear();
+            allUnitsOfMeasures.Clear();
+
+            Console.WriteLine("Recipe cleared");
         }
+        /*
+         * This function allows the user to add as many steps as they want/needed
+         */
         public void addSteps(List<string> allSteps)
         {
             String stepDescription;
@@ -46,42 +63,64 @@ namespace poePart1
             Console.WriteLine("How many steps are there? ");
             int noOfSteps = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i < noOfSteps; i++)
+            for (int i = 0; i < noOfSteps; i++)//Iterates over the amount of steps required and allows user input for each step
             {
                 Console.WriteLine("What is the description of step " + (i + 1));
                 stepDescription = Console.ReadLine();
 
-                allSteps[i] = stepDescription;
+                allSteps.Add(stepDescription);
             }
         }
-        public void displayRecipe(List<string> allIngrediants, List<string> allSteps) 
+        /*
+         * This functions displays the recipe with all array elements with the use of a for loop 
+         */
+        public void displayRecipe(List<string> allIngrediantNames, List<string> allSteps, List<int> allQuantities,
+            List<string> allUnitOfMeasures) 
         {
-            Console.WriteLine("Recipe:\n\n");
+            Console.WriteLine("Recipe:\n");
 
-            for (int i = 0; i < allIngrediants.Count; i++)
+            Console.WriteLine("Ingrediants:");
+            for (int i = 0; i < allIngrediantNames.Count; i++)
             {
-                Console.WriteLine("Ingrediants:\n");
-                Console.WriteLine(allIngrediants[i]);
+                Console.WriteLine(allIngrediantNames[i] + " " + allQuantities[i] + "" + allUnitOfMeasures[i]);
             }
+            Console.WriteLine("\n");
+            Console.WriteLine("Steps:");
             for (int i = 0; i < allSteps.Count; i++)
             {
-                Console.WriteLine("Steps:\n");
                 Console.WriteLine("Step " + (i+1) + ": " + allSteps[i]);
             }
         }
-        public void scaleRecipe(List<string> allIngrediants) 
+
+        /*
+         * This function scales the recipe to the number of people (scaleFactor) required and replaces the new value in the index
+         * of the old value
+         */
+
+        public void scaleRecipe(List<int> allQuantities) 
         {
             Console.WriteLine("What factor would you like to scale the recipe by? E.g 3");
             scaleFactor = Convert.ToInt32(Console.ReadLine());
 
-            string temp = allIngrediants.Find("1-10000");
+            for (int i = 0; i < allQuantities.Count; i++)
+            {
+                int value = allQuantities[i] * scaleFactor;
 
-
+                allQuantities[i] = value;
+            }
         }
-        public void resetScale()
+        /*
+         * This function resets the quantities of the ingrediants back to its original value and replaces the old scaled value index
+         * position with the orginal value
+         */
+        public void resetScale(List<int> allQuantities)
         {
-            scaleFactor = originalScaleFactor;
-        }
+            for (int i = 0; i < allQuantities.Count; i++)
+            {
+                int value = allQuantities[i] / scaleFactor;
 
+                allQuantities[i] = value;
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,40 +4,68 @@
     {
         static void Main(string[] args)
         {
-            List<string> allIngrediants = new List<string>();
+            //Declarations
+            List<string> allIngrediantNames = new List<string>();
             List<string> allSteps = new List<string>();
+            List<int> allQuantities = new List<int>();
+            List<string> allUnitsOfMeasure = new List<string>();
 
-            Methods mt = new Methods();
+            int choice;
+
+            Methods mt = new Methods(); //Object used to call methods from class methods 
 
             Console.WriteLine("Welcome! Please select one of the following options to create your recipe: " +
-                "\n1: Add Ingrediants\n2: Add steps\n3: Display recipe\n4: Scale recipe\n5: Reset Scale\n 6: Clear recipe\n7: Exit");
+                "\n0: Add Ingrediants\n1: Add steps\n2: Display recipe\n3: Scale recipe\n4: Reset Scale\n5: Clear recipe\n6: Exit");
 
-            int choice = Convert.ToInt32(Console.ReadLine());
+            choice = Convert.ToInt32(Console.ReadLine());
 
-            choice--;
-
-            switch (choice)
+            while(choice != 6 ) //performs switch case until the value 6 is entered to stop the program
             {
-                case 0:
-                    mt.addIngrediants(allIngrediants);
-                    break;
+                switch (choice) 
+                {
+                    case 0:
+                        //Calls method to add all ingradiants with its parameters 
+                        mt.addIngrediants(allIngrediantNames, allQuantities, allUnitsOfMeasure);
+                        break;
 
-                case 1:
-                    mt.addSteps(allSteps);
-                    break;
+                    case 1:
+                        //Calls method to add all steps with its parameters 
+                        mt.addSteps(allSteps);
+                        break;
 
-                case 2:
-                    mt.displayRecipe(allIngrediants, allSteps);
-                    break;
+                    case 2:
+                        //Calls method to display the entire recipe with its parameters 
+                        mt.displayRecipe(allIngrediantNames, allSteps, allQuantities, allUnitsOfMeasure);
+                        break;
 
-                case 3:
-                    mt.scaleRecipe();
-                    break;
+                    case 3:
+                        //Calls method to scale the quantities of the recipe with its parameters 
+                        mt.scaleRecipe(allQuantities);
+                        break;
 
-                case 4:
-                    mt.resetScale();
-                    break;
+                    case 4:
+                        //Calls method to reset the recipe quantities back to its original form with its parameters 
+                        mt.resetScale(allQuantities);
+                        break;
 
+                    case 5:
+                        //Calls method to clear the entire recipe with its parameters 
+                        mt.clearRecipe(allIngrediantNames, allSteps, allQuantities, allUnitsOfMeasure);
+                        break;
+
+                    case 6:
+                        //Exit statement 
+                        Console.WriteLine("Thank you for using this application! ");
+                        break;
+
+                    default:
+                        //Shows user to select a correct option 
+                        Console.WriteLine("Please select a valid option.");
+                        break;
+                }
+                Console.WriteLine("Welcome! Please select one of the following options to create your recipe: " +
+                "\n0: Add Ingrediants\n1: Add steps\n2: Display recipe\n3: Scale recipe\n4: Reset Scale\n5: Clear recipe\n6: Exit");
+                choice = Convert.ToInt32(Console.ReadLine());
             }
         }
     }
